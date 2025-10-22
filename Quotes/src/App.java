0de617815aab581d6b797cafdb36f7aaaefabd7e
haywarepiispa <229503043+haywarepiispa@users.noreply.github.com>
2025-10-22 03:35:53 +0300

    InspirationalQuotesV0.2
    Added:
    = Swedish welcome text
    = fix to make it more easily readable
    = First quote in (ENG, FIN, SWE)
    = Timer (try, catch) to make it less
        overwhelming

--- Quotes/src/App.java
+++ Quotes/src/App.java
@@ -1,7 +1,31 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("ENG: Of course, make your own opinions about these!");
+        System.out.println("-");
         System.out.println("FI: Toki, tee omat johtopäätöksesi näistä");
+        System.out.println("-");
+        System.out.println("SWE: Självklart, bilda dig din egen uppfattning om dessa");
+        
+        try {
+
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    
+        System.out.println("Old leaves fall, to be nutrients to fresh leaves");
+        System.out.println("-");
+        System.out.println("Vanhat lehdet putoaa, ollakseen ravinteita tuoreille lehdille");
+        System.out.println("-");
+        System.out.println("Gamla löv faller, för att bli näring åt nya löv");
+    
+        try {
+
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    
     
     
     
