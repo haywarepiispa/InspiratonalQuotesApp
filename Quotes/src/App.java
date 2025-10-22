aa0bc6c951167ee71b836b7b9d9a4c450573afbc
haywarepiispa <229503043+haywarepiispa@users.noreply.github.com>
2025-10-22 03:07:11 +0300

    InspirationalQuotesV0.1
    First patch
    - Added
    = Finnish welcoming text
    = English welcoming text

---
+++ Quotes/src/App.java
@@ -0,0 +1,14 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("ENG: Of course, make your own opinions about these!");
+        System.out.println("FI: Toki, tee omat johtopäätöksesi näistä");
+    
+    
+    
+    
+    
+    
+    
+    
+    }
+}
